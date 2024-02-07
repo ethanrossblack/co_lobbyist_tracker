cbc453459449d03dd0056f8a7572a32c8a6abeeb
Ethan Black <ethanrossblack@gmail.com>
2024-02-06 21:18:11 -0700

    feat: remove lobbyist_id index from schema

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_06_234206) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["primary_lobbyist_id"], name: "index_lobbyists_on_primary_lobbyist_id", unique: true
   end
 
 end
