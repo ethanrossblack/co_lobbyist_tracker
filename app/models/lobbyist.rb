9244197ac045351f8e0cdea7fd35892161995f2e
Ethan Black <ethanrossblack@gmail.com>
2024-02-07 19:05:21 -0700

    feat: add some instance methods to Lobbyist model

--- app/models/lobbyist.rb
+++ app/models/lobbyist.rb
@@ -1,4 +1,15 @@
 class Lobbyist < ApplicationRecord
   has_many :clients, dependent: :destroy
   has_many :incomes, dependent: :destroy
+
+  def total_income
+    incomes.sum(:amount)
+  end
+
+  def top_client
+    clients.joins(:incomes)
+           .group('clients.id')
+           .select('clients.name, SUM(incomes.amount) AS total_paid')
+           .order('total_paid DESC').first
+  end
 end
