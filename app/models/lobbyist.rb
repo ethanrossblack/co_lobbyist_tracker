1d20817e20f2629e7763eff2098c745c43fe41ca
Ethan Black <ethanrossblack@gmail.com>
2024-02-09 18:39:46 -0700

    feat: lobbyist#clients_by_income_amount instance method and test

--- app/models/lobbyist.rb
+++ app/models/lobbyist.rb
@@ -7,10 +7,14 @@ class Lobbyist < ApplicationRecord
   end
 
   def highest_paying_client
+    clients_by_income_amount.first
+  end
+
+  def clients_by_income_amount
     clients.joins(:incomes)
           .where("incomes.lobbyist_id = '#{id}'")
           .group('clients.id')
           .select('clients.*, SUM(incomes.amount) AS total_paid')
-          .order('total_paid DESC').first
+          .order('total_paid DESC')
   end
 end
--- spec/models/lobbyist_spec.rb
+++ spec/models/lobbyist_spec.rb
@@ -37,5 +37,20 @@ RSpec.describe Lobbyist, type: :model do
         expect(@lobbyist_1.highest_paying_client.total_paid).to eq(10000000)
       end
     end
+
+    describe '#clients_by_income_amount' do
+      it "returns a collection of a lobbyist's clients grouped and sorted by the sum of income from client to lobbyist" do
+        expect(@lobbyist_1.clients_by_income_amount.length).to eq(1)
+        expect(@lobbyist_1.clients_by_income_amount.first).to eq(@client_1)
+        expect(@lobbyist_1.clients_by_income_amount.first.total_paid).to eq(3000)
+        
+        client_2 = create(:client, lobbyist: @lobbyist_1)
+        big_income = create(:income, amount:10000000, lobbyist: @lobbyist_1, client: client_2)
+        
+        expect(@lobbyist_1.clients_by_income_amount.length).to eq(2)
+        expect(@lobbyist_1.clients_by_income_amount.first).to eq(client_2)
+        expect(@lobbyist_1.clients_by_income_amount.first.total_paid).to eq(10000000)
+      end
+    end
   end
 end
