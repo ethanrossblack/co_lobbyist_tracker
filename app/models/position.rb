2be27c2cd0b58a157b84d5e7b44b769c7d8ebf3e
Ethan Black <ethanrossblack@gmail.com>
2024-03-26 21:09:33 -0600

    feat: added enum for position attribute in Position model

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,4 +2,11 @@ class Position < ApplicationRecord
   belongs_to :bill
   belongs_to :lobbyist
   belongs_to :client
+
+  enum position: {
+    "Supporting": 0,
+    "Opposing": 1,
+    "Amending": 2,
+    "Monitoring": 3
+  }
 end
